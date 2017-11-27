16d90fb6d3ac208cf2f9aa05ef4b9242fc7af318
Unknown <mgijon94@gmail.com>
2017-11-27 13:07:55 +0100

    igraph example 5

---
+++ Graphics/Examples/igraph_example5.R
@@ -0,0 +1,20 @@
+# library
+library(igraph)
+
+# create data:
+links = data.frame(
+  source = c("A","A", "A", "A", "A","J", "B", "B", "C", "C", "D","I"),
+  target = c("B","B", "C", "D", "J","A","E", "F", "G", "H", "I","I")
+)
+
+# Turn it into igraph object
+network=graph_from_data_frame(d = links, directed = F) 
+
+# Count the number of degree for each node:
+deg=degree(network, mode = "all")
+
+# Plot
+plot(network, vertex.size = deg * 6, vertex.color = rgb(0.1,0.7,0.8,0.5) )
+
+
+# source : https://www.r-graph-gallery.com/251-network-with-node-size-based-on-edges-number/
