7a653d9650903cb038faa4157a28a12afd9cd1d1
Unknown <mgijon94@gmail.com>
2017-11-27 13:06:54 +0100

    igraph example 3

---
+++ Graphics/Examples/igraph_example3.R
@@ -0,0 +1,19 @@
+# library
+library(igraph)
+
+# Create data
+data = matrix(sample(0:1, 400, replace = TRUE, prob = c(0.8,0.2)), nrow = 20)
+network = graph_from_adjacency_matrix(data , mode = 'undirected', diag = F )
+
+# When ploting, we can use different layouts:
+par(mfrow = c(2,2), mar = c(1,1,1,1))
+plot(network, layout = layout.sphere, main = "sphere")
+plot(network, layout = layout.circle, main = "circle")
+plot(network, layout = layout.random, main = "random")
+plot(network, layout = layout.fruchterman.reingold, main = "fruchterman.reingold")
+
+# See the complete list with
+help(layout)
+
+
+# source : https://www.r-graph-gallery.com/247-network-chart-layouts/
