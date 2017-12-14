cb27fa50be2d05ed577f54357a0d60f0022c8dec
Unknown <mgijon94@gmail.com>
2017-12-14 12:54:40 +0100

    Fusión de bases de datos
    Ejemplos varios

---
+++ Basics/Fusión_de_bases_de_datos.R
@@ -0,0 +1,49 @@
+A <- data.frame(id = c("A", "B", "C", "D"), age = c(24, 25, 17, 19), height = c(1.8, 
+                                                                                1.9, 1.75, 1.65))
+A
+B <- data.frame(gender = c("M", "M", "F", "F"), id = c("A", "B", "C", "D"))
+B
+
+C <- data.frame(id = c("A", "B", "C", "D"), math = c(6.5, 8.9, 7.4, 9.2), science = c(7.2, 
+                                                                                      8.4, 6.5, 8.7))
+C
+
+D <- data.frame(id = c("A", "B", "C", "D"), eyes = c("blue", "brown", "green", 
+                                                     "black"))
+
+D
+
+# Nótese que todos ellos tienen como variable identificadora id
+
+## Uniendo los dataframes con merge
+AB <- merge(A, B)  # une A con B
+ABC <- merge(AB, C)  # a la unión de A y B le agrega C
+ABCD <- merge(ABC, D)  # a la unión de A, B y C le agrega D
+ABCD  # resultado final
+
+Reduce(merge, list(A, B, C, D))
+
+####### otro ejemplo
+
+
+authors <- data.frame(surname = I(c("Tukey", "Venables", "Tierney", "Ripley", 
+                                    "McNeil")), nationality = c("US", "Australia", "US", "UK", "Australia"), 
+                      deceased = c("yes", rep("no", 4)))
+
+books <- data.frame(name = I(c("Tukey", "Venables", "Tierney", "Ripley", "Ripley", 
+                               "McNeil", "R Core")), title = c("Exploratory Data Analysis", "Modern Applied Statistics ...", 
+                                                               "LISP-STAT", "Spatial Statistics", "Stochastic Simulation", "Interactive Data Analysis", 
+                                                               "An Introduction to R"), other.author = c(NA, "Ripley", NA, NA, NA, NA, 
+                                                                                                         "Venables & Smith"))
+
+colnames(authors)[1] <- "name"  # cambiando el ID de authors para que sea igual para todos
+
+edition <- data.frame(name = authors[, 1], edition = c(4, 2, 3, 1, 2))  # invento
+year <- data.frame(name = authors[, 1], year = 2000:2004)  # invento
+
+
+m1 <- merge(authors, books)
+m2 <- merge(m1, edition)
+(m3 <- merge(m2, year))  # El resultado final es:
+
+Reduce(merge, list(authors, books, edition, year))
